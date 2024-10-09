d6affb535405f22533ab4fba8249586ba9ecef34
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-09 14:12:35 +1100

    laptop commit check

--- week3_classical_orbital_elements.m
+++ week3_classical_orbital_elements.m
@@ -1,6 +1,6 @@
 % Position and Velocity Vector Input ([xI, yJ, zK] format) [km]
 positionVector = [-1106, 445.2, 6886];
-velocityVector = [-1.897, 7.275, -0.7853]; 
+velocityVector = [-1.897, 7.275, -0.7853];
 mu = 398600; % earth specific
 
 % Find all six COEs (units: km for distance and degrees for angles)
@@ -22,34 +22,34 @@ eccentricityMagnitude = sqrt(sum(eccentricityVector.^2));
 % Finding inclincation, i
 angularMomentumVector = cross(positionVector, velocityVector);
 angularMomentumMagnitude = sqrt(sum(angularMomentumVector.^2)); % h
-inclination = rad2deg(acos(angularMomentumVector(3)/angularMomentumMagnitude));
+inclination = rad2deg(acos(angularMomentumVector(3)/angularMomentumMagnitude)); % deg
 
 % Finding RAAN, bigOmega
 kVector = [0, 0, 1];
 NVector = cross(kVector, angularMomentumVector);
 NMagnitude = sqrt(sum(NVector.^2));
 
-RAAN = rad2deg(acos(NVector(1)/NMagnitude));
+RAAN = rad2deg(acos(NVector(1)/NMagnitude)); % deg
 if NVector(2) < 0
     RAAN = 360 - RAAN;
 end
 
 % Finding argument of perigee, omega
-argumentOfPerigee = rad2deg(acos((dot(NVector, eccentricityVector))/(NMagnitude*eccentricityMagnitude)));
+argumentOfPerigee = rad2deg(acos((dot(NVector, eccentricityVector))/(NMagnitude*eccentricityMagnitude))); % deg
 if eccentricityVector(3) < 0
     argumentOfPerigee = 360 - argumentOfPerigee;
 end
 
 % Finding true anomaly, theta
-trueAnomaly = rad2deg(acos((dot(eccentricityVector, positionVector))/(eccentricityMagnitude * positionMagnitude)));
+trueAnomaly = rad2deg(acos((dot(eccentricityVector, positionVector))/(eccentricityMagnitude * positionMagnitude))); % deg
 if dot(positionVector, velocityVector) < 0
     trueAnomaly = 360 - trueAnomaly;
 end
 
 % Bound status
-boundStatus = 'outbound';
+boundStatus = 'outbound (perigee to apogee)';
 if cross(eccentricityVector, positionVector) < 0
-    boundStatus = 'inbound';
+    boundStatus = 'inbound (apogee to perigee)';
 end
 
 % Output
@@ -59,4 +59,4 @@ fprintf('Inclination,                         i = %.4f°\n', inclination);
 fprintf('Right ascension of ascending node,   Ω = %.4f°\n', RAAN);
 fprintf('Argument of perigee,                 ω = %.4f°\n', argumentOfPerigee);
 fprintf('True anomaly,                        θ = %.4f°\n', trueAnomaly);
-fprintf('Inbound or Outbound,                 %s\n', boundStatus);
+fprintf('Bounding status = %s\n', boundStatus);
