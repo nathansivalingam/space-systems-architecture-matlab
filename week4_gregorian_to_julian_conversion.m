6478af1904034a9e149c8d4dbda9a556350d6d6e
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-11 21:13:07 +1100

    changed the converter slightly because I understand that JD is the Julian day (date and time component), not J_0

--- week4_gregorian_to_julian_conversion.m
+++ week4_gregorian_to_julian_conversion.m
@@ -6,11 +6,11 @@ hour = 02;
 minute = 45;
 second = 30;
 
-% Julian day number formula
+% Julian Day (Date Component)
 J_0 = 367 * year - fix(7 * (year + fix((month + 9)/12))/4) + fix((275 * month)/9) + day + 1721013.5;
 
-% JD (What is it?)
+% Julian Day (Factoring in the Time Component)
 JD = J_0 + ((((second/60) + month)/60) + hour)/24;
 
-fprintf('Julian day number, J_0 = %.1f days\n', J_0);
-fprintf('JD                     = %.1f days\n', JD);
+% Output
+fprintf('Julian Day Number, JD = %.1f days\n', JD);
