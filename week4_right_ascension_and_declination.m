27048bdb65e3660ce72f49096c2242d05e3357a6
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-12 16:02:36 +1100

    transformation from it jt kt to ijk and vis versa (need to figure out how to do it in vis versa)

---
+++ week4_right_ascension_and_declination.m
@@ -0,0 +1,22 @@
+% Input
+positionVector = [-3000, -6000, -9000]; % r = Xi - Yj - Zk;
+
+% Functions
+positionMagnitude = sqrt(positionVector(1).^2 + positionVector(2).^2 + positionVector(3).^2);
+l = positionVector(1)/positionMagnitude;
+m = positionVector(2)/positionMagnitude;
+n = positionVector(3)/positionMagnitude;
+
+% right ascension and declination
+declination = asin(n);
+rightAscension = rad2deg(acos(l/cos(declination)));
+
+if m <= 0
+    rightAscension = 360 - rightAscension;
+end
+
+% Output
+fprintf('Ascension,   α = %.2f°\n', rightAscension);
+fprintf('Declination, δ = %.2f°\n', rad2deg(declination));
+
+% Method is based on the 'Transformation between I_t J_t and K_t & IJK' excel section
