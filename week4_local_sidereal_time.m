6cf72f51bd4d1726cc5807e2cc3b4cbb9989cc58
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-12 15:06:43 +1100

    abstracted a function to avoid repetitiveness

--- week4_local_sidereal_time.m
+++ week4_local_sidereal_time.m
@@ -8,26 +8,27 @@ second = 00;
 
 eastLongitude = 151.2093; % degrees
 
-% Julian Day (Date Component)
+% [STEP 1] Julian Day (Date Component)
 J_0 = 367 * year - fix(7 * (year + fix((month + 9)/12))/4) + fix((275 * month)/9) + day + 1721013.5;
 
-% T_0
+% [STEP 2] T_0
 T_0 = (J_0 - 2451545)/36525;
 
-% Greenwich sidereal time at 0h UT (theta_G_0)
-theta_G_0 = 100.4606184 + 36000.77004 * T_0 + 0.000387933 * T_0.^2 - 2.583 * 10.^-8 * T_0.^3;
-
-while theta_G_0 > 360
-    theta_G_0 = theta_G_0 - 360;
+% 0-to-360 Range Function
+function angle = rangeAdjust(angle)
+    while angle > 360
+        angle = angle - 360;
+    end
 end
 
-% Greenwich sidereal time at any other UT (theta_G)
+% [STEP 3] Greenwich sidereal time at 0h UT (theta_G_0)
+theta_G_0 = 100.4606184 + 36000.77004 * T_0 + 0.000387933 * T_0.^2 - 2.583 * 10.^-8 * T_0.^3;
+theta_G_0 = rangeAdjust(theta_G_0);
+
+% [STEP 4] Greenwich sidereal time at any other UT (theta_G)
 UT = hour + (minute/60) + (second/60.^2);
 theta_G = theta_G_0 + 360.98564724 * (UT/24);
-
-while theta_G > 360
-    theta_G = theta_G - 360;
-end
+theta_G = rangeAdjust(theta_G);
 
 % Local sidereal time (theta)
 theta = theta_G + eastLongitude;
