f4ff7872122d01024208b782a70edc98d7021052
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-19 18:21:23 +1100

    just realised I have to fill out the template almost completely - is highkey barely a template, just a file with some variable names :/ kmn

--- week5_lecture_example.m
+++ week5_lecture_example.m
@@ -33,79 +33,79 @@ fprintf('e = %.5f\n', e);
 fprintf('h = %.2f km^2/s\n', h);
 fprintf('T = %.1f s\n', T);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 2: Calculate nodal regression rate and apsidal rotation rate
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Write your code here
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 2: Calculate nodal regression rate and apsidal rotation rate
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Write your code here
 
 
 
-% Display the results
-fprintf('Nodal regression rate: %.10f °/s\n', rad2deg(Omega_dot));
-fprintf('Apsidal rotation rate: %.10f °/s\n', rad2deg(omega_dot));
+% % Display the results
+% fprintf('Nodal regression rate: %.10f °/s\n', rad2deg(Omega_dot));
+% fprintf('Apsidal rotation rate: %.10f °/s\n', rad2deg(omega_dot));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 3: Calculate the initial mean anomaly and time until perigee
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Additional calculations for initial mean anomaly and time until perigee
-%% Write your code here
-
-
-
-% Display the results
-fprintf('E_0 = %.4f rad\n', E_0);
-fprintf('M_0 = %.4f rad\n', M_0);
-fprintf('t_0 = %.1f s (%.1f s until perigee)\n', t_0, abs(t_0));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 4: Calculate the orbital elements at the given time
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Write your code here
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 3: Calculate the initial mean anomaly and time until perigee
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Additional calculations for initial mean anomaly and time until perigee
+% %% Write your code here
 
 
 
+% % Display the results
+% fprintf('E_0 = %.4f rad\n', E_0);
+% fprintf('M_0 = %.4f rad\n', M_0);
+% fprintf('t_0 = %.1f s (%.1f s until perigee)\n', t_0, abs(t_0));
 
-% Display the results
-fprintf('M = %.5f rad\n', M);
-fprintf('E = %.5f rad\n', E);
-fprintf('theta = %.3f deg\n', rad2deg(theta));
-fprintf('Omega = %.3f deg\n', rad2deg(Omega));
-fprintf('omega = %.3f deg\n', rad2deg(omega));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 5: Calculate the position vector in the geocentric equatorial frame
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Write your code here
-
-
-
-% Displaying the result
-fprintf('r_x = [%f, %f, %f] km\n', r_X_geocentric);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 6: Calculate the position vector in the geocentric ECEF frame
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Write your code here
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 4: Calculate the orbital elements at the given time
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Write your code here
 
 
 
 
-fprintf('theta_E = %.3f deg\n', rad2deg(theta_E));
-%% Write your code here
+% % Display the results
+% fprintf('M = %.5f rad\n', M);
+% fprintf('E = %.5f rad\n', E);
+% fprintf('theta = %.3f deg\n', rad2deg(theta));
+% fprintf('Omega = %.3f deg\n', rad2deg(Omega));
+% fprintf('omega = %.3f deg\n', rad2deg(omega));
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 5: Calculate the position vector in the geocentric equatorial frame
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Write your code here
 
 
 
-% Displaying the result
-fprintf('r_x'' = [%f, %f, %f] km\n', r_X_ECEF);
+% % Displaying the result
+% fprintf('r_x = [%f, %f, %f] km\n', r_X_geocentric);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 7: Calculate the geodetic latitude and longitude
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Write your code here
-
-
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 6: Calculate the position vector in the geocentric ECEF frame
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Write your code here
 
 
-% Printing the result
-fprintf('Latitude: phi = %.2f deg\n', rad2deg(phi));
-fprintf('Longitude: lambda = %.2f deg\n', rad2deg(lambda_));
+
+
+% fprintf('theta_E = %.3f deg\n', rad2deg(theta_E));
+% %% Write your code here
+
+
+
+% % Displaying the result
+% fprintf('r_x'' = [%f, %f, %f] km\n', r_X_ECEF);
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Step 7: Calculate the geodetic latitude and longitude
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Write your code here
+
+
+
+
+% % Printing the result
+% fprintf('Latitude: phi = %.2f deg\n', rad2deg(phi));
+% fprintf('Longitude: lambda = %.2f deg\n', rad2deg(lambda_));
