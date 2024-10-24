87b3936c8549eb696ee05c1efcf625000e14083a
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-10-24 18:58:35 +1100

    week 3 example question done

--- Week 3 Orbital Elements/week3_classical_orbital_elements.m
+++ Week 3 Orbital Elements/week3_classical_orbital_elements.m
@@ -17,31 +17,45 @@ eccentricityVector = [];
 for i = 1:length(positionVector)
     eccentricityVector(i) = (1/mu) * ((velocityMagnitude.^2 - (mu/positionMagnitude)) * positionVector(i) - (dot(positionVector, velocityVector) * velocityVector(i))); %#ok<SAGROW>
 end
+% UNCOMMENT TO VIEW ECCENTRICITY VECTOR
+% fprintf('Eccentricity Vector, e_bold: %.4f I %.4f J %.4f K\n', eccentricityVector(1), eccentricityVector(2), eccentricityVector(3));
 eccentricityMagnitude = sqrt(sum(eccentricityVector.^2));
 
 % Finding inclincation, i
 angularMomentumVector = cross(positionVector, velocityVector);
+% UNCOMMENT TO VIEW ANGULAR MOMENTUM VECTOR
+% fprintf('Angular Momentum Vector, h_bold: %.4f I %.4f J %.4f K\n', angularMomentumVector);
 angularMomentumMagnitude = sqrt(sum(angularMomentumVector.^2)); % h
+% UNCOMMENT TO VIEW ANGULAR MOMENTUM MAGNITUDE
+% fprintf('Angular Momentum Magnitude, h: %.4f\n', angularMomentumMagnitude);
 inclination = rad2deg(acos(angularMomentumVector(3)/angularMomentumMagnitude)); % deg
 
 % Finding RAAN, bigOmega
 kVector = [0, 0, 1];
 NVector = cross(kVector, angularMomentumVector);
+% UNCOMMENT TO N VECTOR
+% fprintf('N Vector, N_bold: %.4f I %.4f J %.4f K\n', NVector);
 NMagnitude = sqrt(sum(NVector.^2));
+% UNCOMMENT TO N MAGNITUDE
+% fprintf('N Magnitude, N: %.4f\n', NMagnitude);
 
+% RAAN Quadrant Check
 RAAN = rad2deg(acos(NVector(1)/NMagnitude)); % deg
+% disp(RAAN);
 if NVector(2) < 0
     RAAN = 360 - RAAN;
 end
 
 % Finding argument of perigee, omega
 argumentOfPerigee = rad2deg(acos((dot(NVector, eccentricityVector))/(NMagnitude*eccentricityMagnitude))); % deg
+% disp(argumentOfPerigee);
 if eccentricityVector(3) < 0
     argumentOfPerigee = 360 - argumentOfPerigee;
 end
 
 % Finding true anomaly, theta
 trueAnomaly = rad2deg(acos((dot(eccentricityVector, positionVector))/(eccentricityMagnitude * positionMagnitude))); % deg
+% disp(trueAnomaly);
 if dot(positionVector, velocityVector) < 0
     trueAnomaly = 360 - trueAnomaly;
 end
