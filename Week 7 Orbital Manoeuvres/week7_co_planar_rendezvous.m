debded2c9a49f576a47be92737a24c81cdf706cb
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-11-04 23:27:29 +1100

    coplanar

---
+++ Week 7 Orbital Manoeuvres/week7_co_planar_rendezvous.m
@@ -0,0 +1,36 @@
+% Planetary Characteristics
+mu = 398600.4; % [Input] (Change when the transfer orbit is between two planets, mu = mu_sun)
+R_0 = 6378; % km [Input]
+
+% (assumed circular orbit)
+h_t = 240; % km [Input]
+h_i = 120; % km [Input]
+r_t = R_0 + h_t; % km
+r_i = R_0 + h_i; % km
+
+% Additional characteristics
+Phi_i = deg2rad(135); % rad [Input]
+
+% TOF of the transfer orbit
+a_t = (r_t + r_i)/2; % km
+fprintf('a_t = %.4f km\n', a_t);
+TOF = pi * sqrt(a_t.^3 / mu); % seconds
+fprintf('TOF = %.4f seconds\n', TOF);
+
+% Angular velocities of the shuttle and satellite
+omega_t = sqrt(mu / r_t.^3); % rad/s
+omega_i = sqrt(mu / r_i.^3); % rad/s
+fprintf('ω_t = %.8f rad/s\n', omega_t);
+fprintf('ω_i = %.8f rad/s\n', omega_i);
+
+% Lead angle
+alpha = omega_t * TOF; % rad
+fprintf('α = %.4f rad\n', alpha);
+
+% Final phase angle
+Phi_f = pi - alpha; % rad
+fprintf('Phi_f = %.4f rad\n', Phi_f);
+
+% Wait time
+WT = (Phi_f - Phi_i)/(omega_t - omega_i);
+fprintf('Wait time = %.4f seconds\n', WT);
