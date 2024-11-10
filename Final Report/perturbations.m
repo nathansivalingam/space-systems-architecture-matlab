e33e5733aded29e44d5620d0dc3452498fa67475
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-11-10 17:01:33 +1100

    created a folder for final report code - might link it in the final report doc

---
+++ Final Report/perturbations.m
@@ -0,0 +1,13 @@
+% ===============================================================================
+% Perturbations
+% ===============================================================================
+%% The Oblateness of the Earth
+% Regression of nodes
+
+% Rotation of apsides
+
+%% Drag
+
+%% Attraction of the sun
+
+%% Solar radiation pressure
