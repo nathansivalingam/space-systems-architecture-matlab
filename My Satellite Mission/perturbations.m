1f7f972c2ed2d81bf759354418c97d154d5dbf71
Nathan Sivalingam <nathansivalingampersonal@gmail.com>
2024-11-14 12:18:03 +1100

    drag

--- My Satellite Mission/perturbations.m
+++ My Satellite Mission/perturbations.m
@@ -18,6 +18,11 @@ omega_dot = 1.0324 * 10.^14 * ( (4 - 5 * (sind(i)).^2) / (a.^(3.5) * (1 - e.^2).
 fprintf('omega_dot = %.4f\n', omega_dot);
 
 %% Drag
+rho = 1.2 * 10.^-11; % kg/m^3
+V = sqrt((2*mu)/a - mu/a); % m / s
+C_d = 2.5;
+A = 2.3 * 3.4; % m^2
+D = (1/2) * rho * V.^2 * C_d * A;
 
 %% Attraction of the sun
 
