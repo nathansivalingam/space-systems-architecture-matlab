e1ea151ed535f960121092b5e753ddb61cf93ff0
Nathan Sivalingam <nathansivalingampersonal@gmail.com>
2024-11-17 14:16:29 +1100

    week 8 main.m and launch_windows.m

---
+++ Test/myFunction.m
@@ -0,0 +1,5 @@
+% File: myFunction.m
+function result = myFunction(x)
+    % This function squares the input number
+    result = x^2;
+end
---
+++ Week 8 Launch Vehicles/main.m
@@ -0,0 +1,4 @@
+i = 28.5;
+L_0 = 28.5;
+result = launchWindowAvailabilityFn(i, L_0);
+disp(result);
