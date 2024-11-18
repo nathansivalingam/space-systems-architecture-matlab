50ca897187745b52b31061a656a92a5aa6fb1f07
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-11-19 01:28:44 +1100

    week 9 lecture 1 done - retained almost nothing except the 3 regions stuff

--- Week 9 Interplanetary Transfers/week9_interplanetary_transfer.m
+++ Week 9 Interplanetary Transfers/week9_interplanetary_transfer.m
@@ -4,4 +4,17 @@
 %% Main Function
 function week9_interplanetary_transfer()
     
+    % REGION 1: SUN-CENTRED TRANSFER
+        % I think it's just a Hohmann's transfer, could be wrong tho
+
+    % REGION 2: EARTH DEPARTURE
+        % 1. Start in circular orbot
+        % 2. Fire the rocket to get a velocity change (boost)
+        % 3. Go into hyperbolic trajectory
+        % 4. At SOI, spacecraft has needed "extra" or "excess" velocity to enter heliocentric elliptical transfer orbit (but no burn)
+        % 5. Hyperbolic trajectory!
+        % 6. Hyperbolic excess speed: the extra velocity needed to go into the transfer orbit at Earth's SOI
+
+    % REGION 3: ARRIVAL AT TARGET PLANET
+
 end
